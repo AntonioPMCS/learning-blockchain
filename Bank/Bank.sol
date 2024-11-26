ea8546e85c3dceedd9b4f99417d4b79160c9e076
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2024-11-26 16:06:32 -0300

    Updates to the Bank and IBank as we did in the class

--- Bank/Bank.sol
+++ Bank/Bank.sol
@@ -4,6 +4,8 @@ pragma solidity >=0.8.2 <0.9.0;
 
 import "./IBank.sol";
 import "@openzeppelin/contracts/interfaces/IERC20.sol";
+// Importar a interface do Oracle
+import "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
 contract Bank is IBank {
 
@@ -12,7 +14,6 @@ contract Bank is IBank {
     constructor () {}
 
     function deposit(uint amount, address token) external {
-
         // Transferir o token do utilizador para o smart contract do banco
         // transferFrom(address from, address to, uint amount)
         IERC20(token).transferFrom(msg.sender, address(this), amount);
@@ -21,15 +22,24 @@ contract Bank is IBank {
     }
 
     function withdraw(uint amount, address token) external {
-        
+        // Garantir que a operação é válida
+        require( (accountBalances[msg.sender])[token] >= amount, "Insufficient balance" ); 
+        // transfer(address to, uint amount)
+        IERC20(token).transfer(msg.sender, amount);
+        // Atualizar o balanço do utilizador para reflectir a transferencia do token
+        (accountBalances[msg.sender])[token] -= amount;
     }
 
-    function exchangeRate(address tokenA, address tokenB) external view {
+    function exchangeRate(address oracle) external view returns (int256) {
 
+        // Ir ao contrato Oracle do ChainLink e devolver o valor
+        (, int256 answer, , , ) = AggregatorV3Interface(oracle).latestRoundData();
+        return answer;
+        // Garantir a pertinencia do exchange Rate recebido
     }
 
     function exchange(address tokenA, address tokenB, uint amount) external {
-
+        
     }
 
     function approvePayment(uint amount, address token) external {
--- Bank/IBank.sol
+++ Bank/IBank.sol
@@ -6,7 +6,7 @@ interface IBank {
 
     function deposit(uint amount, address token) external;
     function withdraw(uint amount, address token) external;
-    function exchangeRate(address tokenA, address tokenB) external view;
+    function exchangeRate(address oracle) external view returns(int256);
     function exchange(address tokenA, address tokenB, uint amount) external;
     function approvePayment(uint amount, address token) external;
     function getBalance(address account, address token) external view returns(uint);
