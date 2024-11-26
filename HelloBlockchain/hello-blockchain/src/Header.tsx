d4a792c69f916e37efbf6e4b61428a0c8746cfb2
AntonioPMCS <apmcarmosilva@ist.utl.pt>
2024-11-26 16:03:34 -0300

    101 Frontend Project

---
+++ HelloBlockchain/hello-blockchain/src/Header.tsx
@@ -0,0 +1,16 @@
+const Header = () => {
+
+
+  function handleClick() {
+    alert("Connect button clicked!");
+  }
+
+  return (
+    <div className = "my-header">
+      <h1>I'm inside Header</h1> 
+      <button onClick={handleClick}>Connect</button> 
+    </div>
+  )
+}
+
+export default Header
---
+++ HelloBlockchain/hello-blockchain/src/Main.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Main.css';
+
+const Main = () => {
+  return (
+    <div className = "my-main">
+      <h1>I'm inside Main</h1>
+    </div>
+  )
+}
+
+export default Main
